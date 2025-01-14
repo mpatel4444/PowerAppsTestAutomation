f09939fd0e34be445b5bb16b70d46c15aba606d6
mpatel4444 <60069621+mpatel4444@users.noreply.github.com>
2025-01-14 14:08:18 +1030

    Update BrowserRecordingEvent.cs

--- Microsoft.PowerApps.TestAutomation.Browser/DTOs/BrowserRecordingEvent.cs
+++ Microsoft.PowerApps.TestAutomation.Browser/DTOs/BrowserRecordingEvent.cs
@@ -21,6 +21,6 @@ namespace Microsoft.PowerApps.TestAutomation.Browser
         public string Text { get; set; }
         public string Value { get; set; }
         public string IFRAME { get; set; }
-        public objcet JObject { get; set; }
+        public objet JObject { get; set; }
     }
 }
